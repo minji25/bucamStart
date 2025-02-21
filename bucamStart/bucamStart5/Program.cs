6d1b89fb44703ed2c2e5a09b169d3279c64a114b
minji25 <zakijeong917@naver.com>
2025-02-21 11:20:26 +0900

    study

---
+++ bucamStart/bucamStart5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucamStart5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자열
+            string greeting; //문자열 변수를 선언
+            greeting = "Hello, World!"; //변수에 값을 저장
+
+            //변수의 값을 사용
+            Console.WriteLine(greeting); //출력 : Hello, World!
+        }
+    }
+}
---
+++ bucamStart/bucamStart6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucamStart6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언과 초기화를 한번에 수행
+            int score = 100; //정수형 변수 선언과 동시에 100으로 초기화
+            double temperature = 36.5f; // 실수형 변수 선언과 초기화
+            string city = "Seoul"; //문자열 변수 선언과 초기화
+
+            //변수 출력
+            Console.WriteLine(score); //출력 100
+            Console.WriteLine(temperature); //출력 36.5
+            Console.WriteLine(city); //출력 : Seoul
+        }
+    }
+}
