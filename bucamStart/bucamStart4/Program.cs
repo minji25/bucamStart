d80e83cdc330dc9228eb7498ae5cbcae0758b470
minji25 <zakijeong917@naver.com>
2025-02-21 10:45:08 +0900

    study4

---
+++ bucamStart/bucamStart4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bucamStart4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string charactername = "Cuphead";
+            int hp = 100;
+            float att = 56.7f;
+            char rank = 'S';
+
+            Console.WriteLine(charactername);
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(rank);
+        }
+    }
+}
